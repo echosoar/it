efcc74b05ccb22b5e4f7ad74613980792bf39881
echosoar <82163514@qq.com>
2017-06-06 18:09:50 -0400

    up

---
+++ test/typeNote.js
@@ -0,0 +1,5 @@
+function cperson(person) {
+    console.log("Hello, " + person);
+}
+var user = "echosoar";
+cperson(user);
---
+++ test/typeNote.ts
@@ -0,0 +1,7 @@
+function cperson(person: string) {
+  console.log("Hello, " + person);
+}
+
+let user = "echosoar";
+
+cperson(user);
